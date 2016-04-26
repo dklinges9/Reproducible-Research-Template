9c0b01b20b55e3c2aab87e20ce5f6c66b2017b78
mindymallory <mallorym@illinois.edu>
2016-04-26 16:58:04 -0500

    simple examples

---
+++ data-raw/fetch-raw-data.R
@@ -0,0 +1,20 @@
+# This file fetches the raw data and performs pre-processing (cleaning) to get it ready for analyzs
+
+library(RCurl)
+library(xts)
+
+# Fetch Corn and Soybean Prices
+CZ2016 <- getURL("https://www.quandl.com/api/v1/datasets/CME/CZ2016.csv")
+SX2016 <- getURL("https://www.quandl.com/api/v1/datasets/CME/SX2016.csv")
+
+
+CZ2016 <- read.csv(text = CZ2016)
+SX2016 <- read.csv(text = SX2016)
+
+# Define first column to be a date.
+CZ2016[,1] <- as.Date(CZ2016[,1])
+SX2016[,1] <- as.Date(SX2016[,1])
+
+# Define the data objects to be xts objects, and keep only settlement prices
+CZ2016 <- xts(CZ2016[,'Settle'], order.by=CZ2016[,1])
+SX2016 <- xts(SX2016[,'Settle'], order.by=SX2016[,1])
