6f6d6ec3b551f3b9cad7553c701dd4d767897d8f
David <klingesd@si.edu>
2021-02-10 11:13:38 -0700

    adjusts to data curation template script, using iris data

--- scripts/04_cleaning_curation/data_curation_template.R
+++ scripts/04_cleaning_curation/data_curation_template.R
@@ -2,7 +2,12 @@
 ## dklinges@ufl.edu
 ## This script provides a template of organization for reproducible workflows
 
+## BAD BAD BAD BAD BAD
+# setwd("Users/David/Desktop")
+
 ## 1. Workspace prep ##############
+
+# Where we load the dependencies of the script (software packages and data)
 
 ## Load packages
 # Workspace/system
@@ -13,23 +18,14 @@ library(tidyverse) # library() returns error, require() returns warning
 library(lubridate)
 # Spatial data
 library(raster)
-library(rgdal)
-
-
-## BAD BAD BAD BAD BAD
-# setwd("Users/David/Desktop")
 
 ## Read in data
-ameriflux_raw <- read_csv("data/raw_data/meteorology/ameriflux/BASE_MeasurementHeight_20190902.csv")
-ameriflux_raw <- read_csv(Sys.glob("*/*/*/*/*/*/BASE_MeasurementHeight_20190902.csv"))
-ameriflux_raw <- read_csv(grepl("BASE_MeasurementHeight_20190902.csv"), list.files)
-
-## Alt + O to collapse all headers
-
-## Load source functions
-source("scripts/01_data_selection/hello_world.R")
-source(Sys.glob("scripts/*_data_selection/hello_world.R"))
-source(here(Sys.glob("scripts/*_data_selection/hello_world.R")))
+iris_raw <- read_csv("data/raw_data/iris.csv") # Pretty standard
+iris_raw <- read_csv(Sys.glob("*/*/iris*.csv")) # Throw in wildcards
+# Harder to read, but possibly useful if you have tons of folders
+iris_raw <- read_csv(list.files(recursive = TRUE)[ # list all files in my project
+  grepl("iris.csv", list.files(recursive = TRUE)) # grepl to get those with "iris.csv"
+  ])
 
 # Written code should be within a margin that most users will be able to view 
 # without having to move their panes around or scroll. This text is cut off at
@@ -40,18 +36,31 @@ source(here(Sys.glob("scripts/*_data_selection/hello_world.R")))
 
 # Tools>Global Options>Code>Display>Show Margin> 60 or 80 are common
 
-
+## Load source functions
+source("scripts/00_source/hello_world.R")
+source(Sys.glob("scripts/*_source/hello_world.R"))
+source(here(Sys.glob("scripts/*_source/hello_world.R")))
 
 ## 2. Data curation #############
 
 ## Make some changes just in case
+
 ## 3. Data QA/QC ##############
 
 ## 4. Write out derived data ###########
 
 # Common practice stemming from when RStudio and other IDE's weren't as popular:
 ####################################################
-############# THIS IS A HEADER #####################
+############# THIS IS A HEADER LOOK AT ME #####################
 ####################################################
 
+## But this is also a header ############
 
+## And this is a header ---------------
+
+
+## Alt + O to collapse all headers on Windows & Linux
+## Alt + Cmd + O to collapse all headers on Mac
+
+## Shift + Alt + O to expand all headers on Windows & Linux
+## Shift + Alt + Cmd + O to collapse all headers on Mac
